00fb084321e8c0c11a75205d5a9cf201777460e1
Bourgoin Théo <theo.bourgoin@hetic.net>
2017-07-04 14:13:59 +0200

    Add create-react-app boilerplate

---
+++ src/components/ContactForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class ContactForm extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            characters: 0
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                sdfs
+            </div>
+        );
+    }
+
+
+}
