e16123ffe398c8bfb96d05fd02dffaaf5a485b45
Bourgoin Théo <theo.bourgoin@hetic.net>
2017-07-04 15:20:04 +0200

    Router is working and the form contact too

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import { Route, Redirect } from 'react-router-dom';
+import HomePage from './pages/home';
+import Layout from './layout/layout';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Redirect from="/" to="/home"/>
+                <Route path="/" component={Layout}/>
+                <Route path="/home" exact component={HomePage}/>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from "../components/ContactForm";
+
+export default class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <ContactForm/>
+            </div>
+        )
+    }
+}
